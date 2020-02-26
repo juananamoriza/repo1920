b5b749787d7164065d18273efb41f0f9dd4f0048
dw1b_alum9 <dw1b_alum9@inform.local>
2020-02-26 13:55:18 +0100

    Version 1

--- tablas/borrar2.sql
+++ tablas/borrar2.sql
@@ -1,3 +1,4 @@
+-- Archivo que borra las 3 tablas
 drop table tcliente2;
 drop table tprovincia2;
 drop table tvendedor2;
--- tablas/crear2.sql
+++ tablas/crear2.sql
@@ -1,3 +1,4 @@
+-- Archivo que crea las 3 tablas
 @creartvendedor2
 @creartprovincia2
 @creartcliente2
